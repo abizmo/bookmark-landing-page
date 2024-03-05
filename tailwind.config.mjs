dbaa80c61ada683355235e2bea9086dc0c10c1ec
abizmo <abizmo@gmail.com>
2024-03-05 13:04:03 +0000

    add html structure

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,10 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    container: {
+      center: true,
+      padding: '1.5rem'
+    },
     extend: {
       colors: {
         primary: {
