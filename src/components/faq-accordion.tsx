017ac41c65721c3eb3a56f297acb4c79be5b8a09
Abián Izquierdo <abizmo@gmail.com>
2024-03-19 14:36:30 +0000

    refactor faq-accordion

--- src/components/faq-accordion.tsx
+++ src/components/faq-accordion.tsx
@@ -4,29 +4,44 @@ import {
   AccordionItem,
   AccordionTrigger
 } from '@/components/ui/accordion'
+import { cn } from '@/lib/utils'
 
-function FaqAccordion() {
+const faqs = [
+  {
+    value: 'faq-1',
+    question: 'What is Bookmark?',
+    answer:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.'
+  },
+  {
+    value: 'faq-2',
+    question: 'How can I request a new browser?',
+    answer:
+      'Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.'
+  },
+  {
+    value: 'faq-3',
+    question: 'Is there a mobile app?',
+    answer:
+      'Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.'
+  },
+  {
+    value: 'faq-4',
+    question: 'What about other Chromium browsers?',
+    answer:
+      'Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.'
+  }
+]
+
+function FaqAccordion({ className }) {
   return (
-    <Accordion type='single' collapsible className='w-full'>
-      <AccordionItem value='item-1'>
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value='item-2'>
-        <AccordionTrigger>Is it styled?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It comes with default styles that matches the other
-          components&apos; aesthetic.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value='item-3'>
-        <AccordionTrigger>Is it animated?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It's animated by default, but you can disable it if you prefer.
-        </AccordionContent>
-      </AccordionItem>
+    <Accordion type='single' collapsible className={cn('w-full', className)}>
+      {faqs.map((faq) => (
+        <AccordionItem key={faq.value} value={faq.value}>
+          <AccordionTrigger>{faq.question}</AccordionTrigger>
+          <AccordionContent>{faq.answer}</AccordionContent>
+        </AccordionItem>
+      ))}
     </Accordion>
   )
 }
